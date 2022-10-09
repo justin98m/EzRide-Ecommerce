45d863e7c376c30a39aeb7ea1463d18ba10e4b9b
Justin C <justin1998m1@gmail.com>
2022-10-09 03:39:21 -0400

    moved location or deleted file

--- javascript/fetch-json.js
+++
@@ -1,9 +0,0 @@
-import data from '../products.json' assert {type: 'json'};
-
-let products = data.products;
-console.log(products);
-products.forEach(product => {
-  console.log(product.name);
-  console.log(product.description);
-  console.log(product.price);
-});
